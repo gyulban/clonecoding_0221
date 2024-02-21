2565d96e24fc9740636616186628a061f4ac3adb
gyulban <park55533@naver.com>
2024-02-21 21:55:00 +0900

    day1

---
+++ workspace/project/project.js
@@ -0,0 +1,59 @@
+//슬라이드 이미지 감싸고 있는 부모요소
+let $bannerBox = $(".banner-box");
+//슬라이드 이미지
+let $bannerImgs = $(".banner-img");
+
+//슬라이드 이미지 너비
+let bannerWidth = 1050;
+//슬라이드 이미지 인덱스 번호
+let currentIdx = 0;
+//총 슬라이드 이미지 수
+let slideCnt = $bannerImgs.length;
+
+$(".next").on("click", moveNext);
+function moveNext() {
+    currentIdx++;
+    $bannerBox.css("left", -(currentIdx * bannerWidth));
+    $bannerBox.css("transition", "0.5s ease");
+    checkEnd();
+}
+
+function moveFirst() {
+    currentIdx = 0;
+    checkEnd();
+    $bannerBox.css("transition", "1.3s ease");
+    $bannerBox.css("left", 0);
+}
+
+$(".prev").on("click", function () {
+    currentIdx--;
+    $bannerBox.css("left", -(currentIdx * bannerWidth));
+    $bannerBox.css("transition", "0.5s ease");
+    checkEnd();
+});
+
+checkEnd();
+
+//처음 이미지와 마지막 이미지는 화살표 감추기
+function checkEnd() {
+    if (currentIdx <= 0) {
+        $(".prev").css("display", "none");
+    } else {
+        $(".prev").css("display", "block");
+    }
+
+    if (currentIdx >= slideCnt - 1) {
+        $(".next").css("display", "none");
+    } else {
+        $(".next").css("display", "block");
+    }
+}
+
+setInterval(function () {
+    if (currentIdx == slideCnt - 1) {
+        // 맨처음으로 돌아갈 수 있도록 함수
+        moveFirst();
+    } else {
+        moveNext();
+    }
+}, 3000);
