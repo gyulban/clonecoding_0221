086effa339eb79513833431e9de8fa26d98abc8b
gyulban <park55533@naver.com>
2024-02-27 19:39:43 +0900

    240227

--- workspace/project1/project.js
+++ workspace/project1/project.js
@@ -112,3 +112,58 @@ function checkEnd2() {
         $(".next-empty").css("display", "none");
     }
 }
+
+//슬라이드 이미지 감싸고 있는 부모요소
+let $bannerBox3 = $(".banner-box-3");
+//슬라이드 이미지
+let $bannerImgs3 = $(".banner-img-3");
+
+//슬라이드 이미지 너비
+let bannerWidth3 = 1115;
+//슬라이드 이미지 인덱스 번호
+let currentIdx3 = 0;
+//총 슬라이드 이미지 수
+let slideCnt3 = $bannerImgs3.length;
+
+$(".nex-text").on("click", moveNext3);
+function moveNext3() {
+    currentIdx3++;
+    $bannerBox3.css("left", -(currentIdx3 * bannerWidth3));
+    $bannerBox3.css("transition", "0.5s ease");
+    checkEnd3();
+}
+
+function moveFirst3() {
+    currentIdx3 = 0;
+    checkEnd3();
+    $bannerBox3.css("transition", "1.3s ease");
+    $bannerBox3.css("left", 0);
+}
+
+$(".pre-text").on("click", function () {
+    currentIdx3--;
+    $bannerBox3.css("left", -(currentIdx3 * bannerWidth3));
+    $bannerBox3.css("transition", "0.5s ease");
+    checkEnd3();
+});
+
+checkEnd3();
+
+//처음 이미지와 마지막 이미지는 화살표 감추기
+function checkEnd3() {
+    if (currentIdx3 <= 0) {
+        $(".pre-text").css("display", "none");
+        $(".pre-empty").css("display", "block");
+    } else {
+        $(".pre-text").css("display", "block");
+        $(".pre-empty").css("display", "none");
+    }
+
+    if (currentIdx3 >= 3) {
+        $(".nex-text").css("display", "none");
+        $(".nex-empty").css("display", "block");
+    } else {
+        $(".nex-text").css("display", "block");
+        $(".nex-empty").css("display", "none");
+    }
+}
